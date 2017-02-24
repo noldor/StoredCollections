2610974731ea9669d23cc20598e1c85116d43c27
Dmitriy Romanov <romanov@noldor.pro>
2017-02-24 19:43:20 +0300

    Change factory

--- src/Factory.php
+++ src/Factory.php
@@ -2,37 +2,33 @@
 
 namespace Noldors\CommerceElements;
 
-
-use Noldors\CommerceElements\DataResolvers\CookiesResolver;
-use Noldors\CommerceElements\DataResolvers\SessionResolver;
 use Noldors\CommerceElements\Exceptions\UnsupportedStorageException;
+use Noldors\CommerceElements\Interfaces\ElementsResolverInterface;
 
 class Factory
 {
-    const SESSION = 1;
-    const COOKIES = 2;
-    const DATABASE = 3;
-
+    /**
+     * Factory constructor.
+     */
     private function __construct()
     {
     }
 
-    public static function create(int $from, string $type)
+    /**
+     * @param string $resolver Resolver class name
+     * @param string $type like 'cart'
+     * @return ElementsResolverInterface
+     * @throws UnsupportedStorageException
+     */
+    public static function create(string $resolver, string $type): ElementsResolverInterface
     {
-        switch ($from) {
-            case self::SESSION:
-                return new SessionResolver($type);
-                break;
-            case self::COOKIES:
-                return new CookiesResolver($type);
-                break;
-            case self::DATABASE:
-                return false;
-                break;
-            default:
-                throw new UnsupportedStorageException();
-                break;
+        $dataResolver = new $resolver($type);
+
+        if ($dataResolver instanceof ElementsResolverInterface) {
+            return $dataResolver;
         }
+
+        throw new UnsupportedStorageException();
     }
 
 }
